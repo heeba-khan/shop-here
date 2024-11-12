dd07b0d69bf6ac3435d73eabe6f1018351c347b4
Heeba Khan <heeba-khan@github.com>
2024-11-12 09:32:33 +0000

    Add logout button

--- shopping-app/src/components/ProtectedLayout.jsx
+++ shopping-app/src/components/ProtectedLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Protected from './Protected'
+import Logout from './Logout'
 
 function ProtectedLayout() {
   return (
     <Protected>
         <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '10px' }}>
-        <LogoutButton />
+        <Logout />
       </div>
         <Outlet/>
     </Protected>
