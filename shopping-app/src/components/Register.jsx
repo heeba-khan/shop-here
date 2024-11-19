668942fab3027ab41b5e9f8bc523402696aab01d
Heeba Khan <heeba-khan@github.com>
2024-11-19 11:45:56 +0000

    Remove unncessary headers from Register.jsx

--- shopping-app/src/components/Register.jsx
+++ shopping-app/src/components/Register.jsx
@@ -19,9 +19,7 @@ const Register = () => {
         method: 'POST',
         headers: {
           "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+          
         },
         body: JSON.stringify({ username, email, password }),
       });
